58dcb1ee270f7ada9cbf6b473a17a7bb253ec74d
olee12 <th160887@gmail.com>
2017-08-01 16:20:26 +0600

    voting system

--- MashroomClassifier.py
+++ MashroomClassifier.py
@@ -20,7 +20,12 @@ encoder['?'] = 27;
 def prepareTrainAndTestData(path):
 
     fileIn = open(path, "r", encoding='utf-8');
-    lines = fileIn.read().split('\n');
+    lines = fileIn.read().split('\n')[1:];
+
+    #print(lines[:10])
+    random.shuffle(lines)
+    #print(lines[:10])
+
 
     allEValues = []
     allEClasses = []
@@ -28,7 +33,7 @@ def prepareTrainAndTestData(path):
     allPClasses = []
     # for i in range(1,27):
     #     print("dict " + str(encoder[chr(i+96)]));
-    for i in range(1,len(lines)):
+    for i in range(0,len(lines)):
         data = lines[i].split(',')
         if(data[0] == 'e'):
             allEClasses.append(data[0])
@@ -123,7 +128,6 @@ def svmC(path):
     #print(trainFeatureValues);
     clf.fit(trainFeatureValues_scale, trainClasses);
     predictedClasses = clf.predict(validationFeatureValues_scale);
-    print("-----------------------------------------------------------------")
     print("--------------------VALIDATION--------------------------")
     print("SVM accuracy score = " + str(accuracy_score(validationClasses, predictedClasses) * 100) + "%")
     print("SVM precision score = " + str(
@@ -132,9 +136,7 @@ def svmC(path):
         recall_score(validationClasses, predictedClasses, average='macro')* 100) + "%")
     print("SVM f1 score = " + str(
         f1_score(validationClasses, predictedClasses, average='macro') * 100) + "%")
-    print("-----------------------------------------------------------------")
     predictedClasses = clf.predict(testFeatureValues2_scale);
-    print("-----------------------------------------------------------------")
     print("--------------------TESTING--------------------------")
     print("SVM accuracy score = " + str(accuracy_score(testClasses2, predictedClasses) * 100) + "%");
     print("SVM precision score = " + str(
@@ -160,6 +162,70 @@ def naiveByes(path):
     print("-----------------------------------------------------------------")
 
 
+def votingSystem(path):
+    trainFeatureValues, trainClasses, testFeatureValues, testClasses = prepareTrainAndTestData(path);
+    clf = MLPClassifier(activation='logistic', alpha=.0001, hidden_layer_sizes=(20, 20, 20,), random_state=False)
+    clf1 = MLPClassifier(activation='relu', alpha=.0001, hidden_layer_sizes=(20, 20, 20,), random_state=False)
+    clf2 = MLPClassifier(activation='logistic', alpha=.0001, hidden_layer_sizes=(15, 15, 15,), random_state=False)
+    clf3 = MLPClassifier(activation='logistic', alpha=.0001, hidden_layer_sizes=(100, 100,), random_state=False)
+    clf4 = MLPClassifier(activation='logistic', alpha=.0001, hidden_layer_sizes=(50, 50,50), random_state=False)
+    clfsv1 = svm.SVC(kernel="linear", C=25);
+    clfnaive = GaussianNB();
+    testClasses2 = testClasses[:int(len(testClasses) * .5)]
+    validationClasses = testClasses[int(len(testClasses) * .5) + 1:]
+    testFeatureValues2 = testFeatureValues[:int(len(testClasses) * .5)]
+    validationFeatureValues = testFeatureValues[int(len(testClasses) * .5) + 1:]
+
+
+    clf.fit(trainFeatureValues, trainClasses)
+    clf1.fit(trainFeatureValues, trainClasses)
+    clf2.fit(trainFeatureValues, trainClasses)
+    clf3.fit(trainFeatureValues,trainClasses)
+    clf4.fit(trainFeatureValues,trainClasses)
+    #clfsv1.fit(trainFeatureValues,trainClasses)
+    #clfnaive.fit(trainFeatureValues,trainClasses)
+    clf_list = [clf,clf1,clf2,clf3,clf4];
+
+    predictedClasses = votingSystemPredict(clf_list,validationFeatureValues);
+    print("--------------------VALIDATION--------------------------")
+    print("Voting system accuracy score = " + str(accuracy_score(validationClasses, predictedClasses) * 100) + "%")
+    print("Voting system precision score = " + str(
+        precision_score(validationClasses, predictedClasses, average='macro') * 100) + "%")
+    print("Voting system recall score = " + str(
+        recall_score(validationClasses, predictedClasses, average='macro') * 100) + "%")
+    print(
+        "Voting system f1 score = " + str(f1_score(validationClasses, predictedClasses, average='macro') * 100) + "%")
+    predictedClasses = votingSystemPredict(clf_list, testFeatureValues2);
+    print("--------------------TESTING--------------------------")
+    print("Voting system accuracy score = " + str(accuracy_score(testClasses2, predictedClasses) * 100) + "%")
+    print("Voting system precision score = " + str(
+        precision_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
+    print("Voting system recall score = " + str(
+        recall_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
+    print("Voting system f1 score = " + str(
+        f1_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
+    print("-----------------------------------------------------------------")
+
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+def votingSystemPredict(clfs,test_data):
+    voting_pred = list();
+    prediction = list();
+    for i in range(0,len(test_data)):
+        voting_pred.append([]);
+
+    for clf in clfs:
+        for i in range(0,len(test_data)):
+            voting_pred[i].append(clf.predict([test_data[i]])[0]);
+    for i in range(0, len(test_data)):
+        prediction.append(most_common(voting_pred[i]));
+
+    #print(prediction);
+    return prediction;
+
+
 def neuralNetwork(path):
     trainFeatureValues, trainClasses, testFeatureValues, testClasses = prepareTrainAndTestData(path);
 
@@ -167,33 +233,33 @@ def neuralNetwork(path):
     validationClasses = testClasses[int(len(testClasses)*.5)+1:]
     testFeatureValues2 = testFeatureValues[:int(len(testClasses) * .5)]
     validationFeatureValues = testFeatureValues[int(len(testClasses) * .5) + 1:]
+
     clf = MLPClassifier(activation='logistic', alpha=.0001, hidden_layer_sizes=(20,20,20,),random_state=False)
-    print(clf);
-    clf.fit(trainFeatureValues, trainClasses);
-    predictedClasses = clf.predict(validationFeatureValues);
-    print("-----------------------------------------------------------------")
+    print(clf)
+    clf.fit(trainFeatureValues, trainClasses)
+    predictedClasses = clf.predict(validationFeatureValues)
     print("--------------------VALIDATION--------------------------")
-    print("Neural network accuracy score = " + str(accuracy_score(validationClasses, predictedClasses) * 100) + "%");
-    print("Neural network precision score = " + str(precision_score(validationClasses, predictedClasses,average='macro') * 100) + "%");
-    print("Neural network recall score = " + str(recall_score(validationClasses, predictedClasses,average='macro') * 100) + "%");
-    print("Neural network f1 score = " + str(f1_score(validationClasses, predictedClasses,average='macro') * 100) + "%");
-    print("-----------------------------------------------------------------")
-    predictedClasses = clf.predict(testFeatureValues2);
-    print("-----------------------------------------------------------------")
+    print("Neural network accuracy score = " + str(accuracy_score(validationClasses, predictedClasses) * 100) + "%")
+    print("Neural network precision score = " + str(precision_score(validationClasses, predictedClasses,average='macro') * 100) + "%")
+    print("Neural network recall score = " + str(recall_score(validationClasses, predictedClasses,average='macro') * 100) + "%")
+    print("Neural network f1 score = " + str(f1_score(validationClasses, predictedClasses,average='macro') * 100) + "%")
+    predictedClasses = clf.predict(testFeatureValues2)
     print("--------------------TESTING--------------------------")
-    print("Neural network accuracy score = " + str(accuracy_score(testClasses2, predictedClasses) * 100) + "%");
+    print("Neural network accuracy score = " + str(accuracy_score(testClasses2, predictedClasses) * 100) + "%")
     print("Neural network precision score = " + str(
-        precision_score(testClasses2, predictedClasses, average='macro') * 100) + "%");
+        precision_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
     print("Neural network recall score = " + str(
-        recall_score(testClasses2, predictedClasses, average='macro') * 100) + "%");
+        recall_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
     print("Neural network f1 score = " + str(
-        f1_score(testClasses2, predictedClasses, average='macro') * 100) + "%");
+        f1_score(testClasses2, predictedClasses, average='macro') * 100) + "%")
     print("-----------------------------------------------------------------")
 
 path = "mushrooms.csv";
 naiveByes(path);
 svmC(path);
 decisionTree(path);
-neuralNetwork(path)
+neuralNetwork(path);
+votingSystem(path);
+
 
 
